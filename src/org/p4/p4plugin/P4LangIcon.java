15d47de8d15f02269e9a8eab53ce930a83601cda
Yi Tseng <a86487817@gmail.com>
2017-11-30 18:32:50 -0800

    Add icon

---
+++ src/org/p4/p4plugin/P4LangIcon.java
@@ -0,0 +1,9 @@
+package org.p4.p4plugin;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.Icon;
+
+public class P4LangIcon {
+    public static final Icon ICON = IconLoader.findIcon("/org/p4/p4plugin/icon/p4-icon.png");
+}
