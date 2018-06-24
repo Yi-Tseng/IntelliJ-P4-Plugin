747cc60096abf01eb3c75147eac837a960117036
Yi Tseng <a86487817@gmail.com>
2018-06-24 17:55:54 +0800

    add tab setting

---
+++ src/org/p4/p4plugin/style/P4LangTabbedCodeStylePanel.java
@@ -0,0 +1,17 @@
+package org.p4.p4plugin.style;
+
+import com.intellij.application.options.TabbedLanguageCodeStylePanel;
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import org.p4.p4plugin.P4Lang;
+
+public class P4LangTabbedCodeStylePanel extends TabbedLanguageCodeStylePanel {
+
+    public P4LangTabbedCodeStylePanel(CodeStyleSettings currentSettings, CodeStyleSettings settings) {
+        super(P4Lang.INSTANCE, currentSettings, settings);
+    }
+
+    @Override
+    protected void initTabs(CodeStyleSettings codeStyleSettings) {
+        addSpacesTab(codeStyleSettings);
+    }
+}
